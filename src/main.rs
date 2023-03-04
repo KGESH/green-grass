3b4be5a8c9f4be37df758feac1400ecc417e271e
KGESH <seonghyeon.jee@gmail.com>
2023-03-04 23:25:28 +0900

    [Feat] 랜덤 반복

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
 mod app;
 
+use rand::Rng;
+use std::thread::sleep;
+use std::time::Duration;
+
 fn main() {
     println!("Start commit ...");
-    app::run();
+
+    let loop_count = rand::thread_rng().gen_range(8..=21);
+
+    for _ in 0..loop_count {
+        app::run();
+        sleep(Duration::from_secs(1));
+    }
+
     println!("Commit done.");
 }
